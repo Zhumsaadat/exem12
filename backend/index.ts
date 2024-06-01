897dc4750f4a2f4807f2093876f2cf9ff1953fb2
Zhumashova Saadat <saadat9195@gmail.com>
2024-06-01 11:40:33 +0300

    1. Добавлена возможность регистрации, логина и логина через google аккаунт. 2. Добавила editorconfig.

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,28 @@
-import express from 'express';
 import cors from 'cors';
-
+import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
+import usersRouter from './routers/users';
 
 const app = express();
 const port = 8000;
-
-app.use(cors());
+app.use(express.static('public'));
 app.use(express.json());
+app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Port: ${port}`);
-});
+app.use('/users', usersRouter);
+
+
+const run = async () => {
+  await mongoose.connect(config.mongoose.db);
+
+  app.listen(port, () => {
+    console.log(`server started on ${port} port`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+void run();
